ddf6cadf9683f5a6747ecbfe3a072709946d848a
Essmann <kenwilliam1000@gmail.com>
2024-12-05 15:49:28 +0100

    Fixed scripts, added a subfolder for it

--- Scripts/initialize.ps1
+++ Scripts/initialize.ps1
@@ -1,11 +1,23 @@
 # Get the directory of the script
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-# Get the parent directory of the script directory
+# Optionally: Get the parent directory of the script directory, which should be the main project directory
 $parentDir = Split-Path -Parent $scriptDir
+
+# Check if the script directory exists as expected
+Write-Host "Script directory is: $scriptDir"
+Write-Host "Parent directory is: $parentDir"
 
 # Get the current PATH environment variable
 $currentPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
+
+# Check if the script directory is already in the PATH
+if ($currentPath -notlike "*$scriptDir*") {
+    # If not, add it to the PATH
+    $newPath = $currentPath + ";" + $scriptDir
+    [System.Environment]::SetEnvironmentVariable('PATH', $newPath, [System.EnvironmentVariableTarget]::User)
+    Write-Host "Added $scriptDir to PATH"
+}
 
 # Check if the parent directory is already in the PATH
 if ($currentPath -notlike "*$parentDir*") {
@@ -14,9 +26,6 @@ if ($currentPath -notlike "*$parentDir*") {
     [System.Environment]::SetEnvironmentVariable('PATH', $newPath, [System.EnvironmentVariableTarget]::User)
     Write-Host "Added $parentDir to PATH"
 }
-
-# Get the current user path after potential modification
-$userPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
 
 # Create env variable for the path so that dotnet run works from anywhere
 if (-not [System.Environment]::GetEnvironmentVariable('rarbg_cli_path', [System.EnvironmentVariableTarget]::User)) {
--- Scripts/torrent.ps1
+++ Scripts/torrent.ps1
@@ -4,7 +4,7 @@ $projectPath = Join-Path $env:rarbg_cli_path "HttpRequests.csproj"
 # Ensure the project file path exists
 if (Test-Path $projectPath) {
     # Run the dotnet project with arguments passed
-    dotnet run --project $projectPath -- @args
+    dotnet run --project $projectPath -- $args
 } else {
     Write-Host "Project file not found at $projectPath"
 }
