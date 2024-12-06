6ddf0fea45708ae84c38f37e19a7a06cc607321c
Essmann <kenwilliam1000@gmail.com>
2024-12-06 12:58:45 +0100

    Removed initializeconfig

--- Classes/Config.cs
+++ Classes/Config.cs
@@ -14,48 +14,6 @@ public static class Config
     public static int MaxPages = 2;
     public static int MaxDisplay = 15;
 
-    public static void InitializeConfig()
-    {
-        try
-        {
-            string cwd = Directory.GetCurrentDirectory();
-            string projectDirectory = Directory.GetParent(cwd).Parent.Parent.FullName;
-            string fileName = "config.txt";
-            string filePath = Path.Combine(projectDirectory, fileName);
-
-            if (!File.Exists(filePath))
-            {
-                File.Create(filePath).Close();
-                Config.SaveToFile(filePath);
-                Console.WriteLine($"Config.txt file created at {cwd}");
-            }
-        }
-        catch
-        {
-
-        }
-    }
-    public static void SaveToFile(string filePath)
-    {
-        var configData = new { BaseUrl, UserAgent, MaxPages, MaxDisplay };
-
-        string jsonString = JsonSerializer.Serialize(
-            configData,
-            new JsonSerializerOptions { WriteIndented = true }
-        );
-
-        using (
-            var fileStream = new FileStream(
-                filePath,
-                FileMode.Create,
-                FileAccess.Write,
-                FileShare.None
-            )
-        )
-        using (var writer = new StreamWriter(fileStream))
-        {
-            writer.Write(jsonString);
-        }
-        Console.WriteLine($"Configuration saved to {filePath}");
-    }
+    
+   
 }
