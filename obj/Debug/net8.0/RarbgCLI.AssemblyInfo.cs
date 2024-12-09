a698b343987cbf5cc2cbe2cfca399d38fbc6447d
Essmann <kenwilliam1000@gmail.com>
2024-12-09 17:56:09 +0100

    Git ignored a lot and reset to dev

--- obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c4164a2f8e0e19470fd758a5cbb37ec8de6f37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381ee819c84c424bfbe635a32268c1fa7c3c8fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
