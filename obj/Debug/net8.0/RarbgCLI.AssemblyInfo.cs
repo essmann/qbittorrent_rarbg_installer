6c789b763e9e3f8c68362c3b202e7ace705c6990
Essmann <kenwilliam1000@gmail.com>
2024-12-09 17:36:14 +0100

    123

--- obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43d86d09b670b154189280e6b17a5fbd94bba77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a01a635189e9ae381b8f6600ac1edb088ef2c07")]
 [assembly: System.Reflection.AssemblyProductAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
