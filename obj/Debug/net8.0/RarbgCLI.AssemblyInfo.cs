6d0ac366ddac242d045e92ed4740049996123827
Essmann <kenwilliam1000@gmail.com>
2024-12-09 15:13:04 +0100

    removed spammy console writelines

--- obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RarbgCLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffad32faa6f17d6eb3f0aa4bc9e6936c6291c944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a393585fc7b0b13a28b75a50e0f0ae8b061cd112")]
 [assembly: System.Reflection.AssemblyProductAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RarbgCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
